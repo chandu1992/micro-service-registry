14202a9a55f036dbcc89cd08b078e1a52eef96f2
chandrashekharbhaupande <chpande55@gmail.com>
2025-06-02 23:41:42 +0530

    data

---
+++ src/test/java/com/jarvis/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
