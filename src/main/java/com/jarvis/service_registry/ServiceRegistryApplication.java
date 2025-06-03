d4aa4c3eaf3c8c784e11cdd8ba1eb698cbd4f454
chandrashekharbhaupande <chpande55@gmail.com>
2025-06-03 23:11:17 +0530

    code

--- src/main/java/com/jarvis/service_registry/ServiceRegistryApplication.java
+++ src/main/java/com/jarvis/service_registry/ServiceRegistryApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class ServiceRegistryApplication {
 
+	/*A Service Registry is a core component in a microservices architecture. Its main role is to keep track of
+	all the available microservices and their instances so that they can find and communicate with each other
+	dynamically.*/
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
